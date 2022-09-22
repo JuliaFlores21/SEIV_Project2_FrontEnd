59544e1e696560608becf0e846df8166f4686482
TaiyouD <111469767+TaiyouD@users.noreply.github.com>
2022-09-22 16:49:19 -0500

    Update http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,18 @@
 import axios from "axios";
+var baseURL = "";
+if (process.env.NODE_ENV === "development") {
+  baseURL = "http://localhost:3002/course-t4/";
+} else {
+  baseURL = "http://project2.eaglesoftwareteam.com/course-t4/";
+}
+
 export default axios.create({
-  baseURL: "http://project2.eaglesoftwareteam.com/course-t4/",
+  baseURL: baseURL,
   headers: {
-    "Content-type": "application/json"
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    "X-Requested-With":"XMLHttpRequest",
+    "Access-Control-Allow-Origin":"*",
+    crossDomain: true
   }
 });

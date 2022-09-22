4cfa336ce539bae9c4f3bd2c4b774da44265bd6b
TaiyouD <111469767+TaiyouD@users.noreply.github.com>
2022-09-22 17:08:34 -0500

    Update http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 var baseURL = "";
-if (process.env.NODE_ENV === "dev") {
+if (process.env.NODE_ENV === "development") {
   baseURL = "http://localhost:3002/course-t4/";
 } else {
   baseURL = "http://project2.eaglesoftwareteam.com/course-t4/";

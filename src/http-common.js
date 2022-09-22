7248e20862181bbaa67f0d92760efc42b6781653
TaiyouD <111469767+TaiyouD@users.noreply.github.com>
2022-09-22 17:01:47 -0500

    Update http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -3,7 +3,7 @@ var baseURL = "";
 if (process.env.NODE_ENV === "dev") {
   baseURL = "http://localhost:3002/course-t4/";
 } else {
-  baseURL = "http://project2.eaglesoftwareteam.com/";
+  baseURL = "http://project2.eaglesoftwareteam.com/course-t4/";
 }
 
 export default axios.create({

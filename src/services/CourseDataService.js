0c0fc2e7dec4e0d34197b23396caef31e55a57dc
cjackson027 <c.jackson@eagles.oc.edu>
2022-09-22 16:12:54 -0500

    working frontend

---
+++ src/services/CourseDataService.js
@@ -0,0 +1,25 @@
+import http from "../http-common";
+class CourseDataService {
+  getAll() {
+    return http.get("/");
+  }
+  get(CourseNumber) {
+    return http.get(`/${CourseNumber}`);
+  }
+  create(data) {
+    return http.post("/", data);
+  }
+  update(CourseNumber, data) {
+    return http.put(`/${CourseNumber}`, data);
+  }
+  delete(CourseNumber) {
+    return http.delete(`/${CourseNumber}`);
+  }
+  deleteAll() {
+    return http.delete(`/courses`);
+  }
+  findByCourseNumber(CourseNumber) {
+    return http.get(`/courses?CourseNumber=${CourseNumber}`);
+  }
+}
+export default new CourseDataService();

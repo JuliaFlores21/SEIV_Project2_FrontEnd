25d38e19ec18751bbf9550b51dacf3d6c675b9d4
TaiyoD <t.dail@eagles.oc.edu>
2022-09-23 09:52:47 -0500

    added course-t4

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,18 @@
 import axios from "axios";
+var baseURL = "";
+if (process.env.NODE_ENV === "development") {
+  baseURL = "http://localhost/course-t4/";
+} else {
+  baseURL = "/course-t4/";
+}
+
 export default axios.create({
-  baseURL: "http://project2.eaglesoftwareteam.com/",
+  baseURL: baseURL,
   headers: {
-    "Content-type": "application/json"
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    "X-Requested-With":"XMLHttpRequest",
+    "Access-Control-Allow-Origin":"*",
+    crossDomain: true
   }
 });
